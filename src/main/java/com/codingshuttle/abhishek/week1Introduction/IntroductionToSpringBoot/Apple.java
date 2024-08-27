ed6b479335b6c9fbb9cd744e059bd68d0883359b
abhi8127 <abhisheky06880@gmail.com>
2024-08-27 16:04:03 +0530

    1.3 Bean

--- src/main/java/com/codingshuttle/abhishek/week1Introduction/IntroductionToSpringBoot/Apple.java
+++ src/main/java/com/codingshuttle/abhishek/week1Introduction/IntroductionToSpringBoot/Apple.java
@@ -1,8 +1,19 @@
 package com.codingshuttle.abhishek.week1Introduction.IntroductionToSpringBoot;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
 public class Apple {
     void eatapple(){
         System.out.println("i am eating the apple");
 
+    }
+@PostConstruct
+    void callThisBeforeAppleIsUsed(){
+        System.out.println("Creating the apple before used");
+    }
+@PreDestroy
+    void callThisBeforeDestroy(){
+        System.out.println("Destroying the apple bean");
     }
 }
