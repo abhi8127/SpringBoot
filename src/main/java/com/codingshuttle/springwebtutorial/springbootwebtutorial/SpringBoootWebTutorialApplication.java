191ba133d91efeeb5eef2bdacc546e43966a0d3d
abhi8127 <abhisheky06880@gmail.com>
2024-09-27 14:25:34 +0530

    Chapter2.1

---
+++ src/main/java/com/codingshuttle/springwebtutorial/springbootwebtutorial/SpringBoootWebTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.springwebtutorial.springbootwebtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoootWebTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoootWebTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingshuttle/springwebtutorial/springbootwebtutorial/SpringBoootWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.springwebtutorial.springbootwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoootWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
